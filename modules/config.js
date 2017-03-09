b59f2a01ac18171a7b77a07f1a8a9600ea1cf631
small-small-girl <1441849713@qq.com>
2017-03-09 15:13:50 +0800

    updata router

--- modules/config.js
+++ modules/config.js
@@ -3,7 +3,7 @@
 var ApplicationConfiguration = (function(){
 	// 应用程序名和依赖
 	var applicationModuleName = 'appname';
-	var applicationModuleVendorDependencies = ['ui.router'];
+	var applicationModuleVendorDependencies = ['ui.router','ui.bootstrap'];
 
 	// 添加新模块
 	var registerModule = function(moduleName, dependencies) {
