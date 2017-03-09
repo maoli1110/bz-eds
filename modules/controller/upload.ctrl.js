a0149517b1d90fde6d14ba9a6f6c66ad78c0195a
li jing <18739570311@163.com>
2017-03-09 18:13:56 +0800

    this is a file

--- modules/controller/upload.ctrl.js
+++ modules/controller/upload.ctrl.js
@@ -3,7 +3,20 @@
  * component
  */
 angular.module('core').controller('uploadCtrl', ['$scope', '$http',
-    function ($scope, commonService, $http) {
+    function ($scope, commonService, $http, $log) {
+        $scope.totalItems = 64;
+        $scope.currentPage = 4;
 
-       
-}]);
+        $scope.setPage = function (pageNo) {
+            $scope.currentPage = pageNo;
+        };
+
+        $scope.pageChanged = function() {
+            $log.log('Page changed to: ' + $scope.currentPage);
+        };
+
+        $scope.maxSize = 5;
+        $scope.bigTotalItems = 175;
+        $scope.bigCurrentPage = 1;
+
+    }]);
