e20447d21140d4e51b56d5791e5fac120f0a689e
maoli1110 <848631812@qq.com>
2017-03-13 15:01:41 +0800

    audit upload

--- modules/controller/aduit.ctrl.js
+++ modules/controller/aduit.ctrl.js
@@ -2,8 +2,51 @@
 /**
  * component
  */
-angular.module('core').controller('auditCtrl', ['$scope', '$http',
-    function ($scope, commonService, $http) {
+angular.module('core').controller('auditCtrl', ['$scope', 'commonService', '$http',function ($scope, commonService, $http) {
+	$scope.sidebar = 'audit';
+    commonService.auditList().then(function(data){
+    	console.log('success');
+    	$scope.auditList = data.data.slice(0,6);
+    	console.log($scope.auditList);
+    });
 
-       
+    /*
+     * 分页器
+     * */
+	$scope.totalItems = 64;
+	$scope.currentPage = 1;
+	$scope.setPage = function (pageNo) {
+	$scope.currentPage = pageNo;
+	};
+	$scope.pageChanged = function() {
+	//console.log('Page changed to: ' + $scope.currentPage);
+	};
+	$scope.maxSize = 5;
+	$scope.bigTotalItems = 175;
+	$scope.bigCurrentPage = 1;
+   
+    /*分页器跳转
+     * params  value
+     * return currentPage
+     * */
+    function getDumpVal(){
+        var dumpVal =  $('.dump-inp input').val();
+        return dumpVal;
+    }
+    
+    function setFilterStyle(obj,event){
+        obj.on(event,function(){
+            $(this).parent().siblings().css({'height':'auto','overflow':''})
+        })
+    }
+
+    /*分页器跳转
+     * params  value
+     * return currentPage
+     * */
+    // $scope.setPage(getDumpVal());
+    $scope.getDumpOk = function(){
+        $scope.setPage(getDumpVal());
+    };
+
 }]);
