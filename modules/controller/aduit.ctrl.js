188316ffd8127b1fc289ffc2ba8a3d038b68dfbb
maoli1110 <848631812@qq.com>
2017-03-14 19:02:37 +0800

    audit upload

--- modules/controller/aduit.ctrl.js
+++ modules/controller/aduit.ctrl.js
@@ -5,12 +5,18 @@
 angular.module('core').controller('auditCtrl', ['$scope', 'commonService', '$http',function ($scope, commonService, $http) {
 	$scope.sidebar = 'audit';
     $scope.flag = {};
-    commonService.auditList().then(function(data){
-    	console.log('success');
-    	$scope.auditList = data.data.slice(0,20);
-    	console.log($scope.auditList);
-    });
-
+    //获取auditList
+    $scope.getAuditList = function (source,currentPage) {
+        if((source == "pass") || (source =="noPass")) {
+            $scope.flag.auditStatus = false;
+        }
+        commonService.auditList().then(function(data){
+            console.log('success');
+            $scope.auditList = data.data.slice(0,20);
+            console.log($scope.auditList);
+        });
+    }
+    $scope.getAuditList(); 
     //分页器
 	$scope.totalItems = 64;
 	$scope.currentPage = 1;
@@ -73,4 +79,38 @@ angular.module('core').controller('auditCtrl', ['$scope', 'commonService', '$htt
     $scope.auditStatusShow = function () {
         $scope.flag.auditStatus = !$scope.flag.auditStatus
     }
+
+    //点击审核的其他区域，收起下拉
+    $(document).click(function(event) {
+        if($(event.target).attr("class") && $(event.target).attr("class").indexOf("pop-signal")!==-1){
+            return false;
+        }
+        if($scope.flag.auditStatus){
+            $scope.flag.auditStatus = false;
+            $scope.$apply();
+        }
+    });
+
+    function  menusEvent(obj,event){
+            $('.menus-childs').unbind('click').click(function(){
+                return false;
+            });
+            obj.off(event).on(event,function(){
+                 $(this).toggleClass('active');
+                 $(this).siblings().removeClass('active');
+                 $(this).siblings().find('.menus-childs').stop().slideUp();
+                 $(".menusName").css({'background':'','color':'#333'});
+                 $(this).siblings().find('.strioke').hide();
+                 $(this).find(".menusName").css({'background':'#666','color':'#fff'});
+                 $(this).find('.strioke').show();
+
+                 if($(this).hasClass('active')){
+                     $(this).find('.menus-childs').stop().slideDown();
+                 }else{
+                     $(this).find('.menus-childs').stop().slideUp();
+                 }
+                     //$(this).find('.menus-childs').slideDown().parent().siblings().find('.menus-childs').slideUp();
+             })
+        }
+
 }]);
