6844ced7dbc438e8dba69eeb2a339310988057b8
li jing <18739570311@163.com>
2017-03-16 16:31:04 +0800

    large

--- modules/controller/largePattern.ctrl.js
+++ modules/controller/largePattern.ctrl.js
@@ -12,24 +12,27 @@ angular.module('core').controller('largePatternCtrl', ['$scope', '$http','$uibMo
         var  siderbarArr = [];//菜单项
         var  filterCount = 1;//筛选开关
         var  dumpVal;//分页器跳转框的值
-        $scope.menusArr = [];//菜单数组
+        var showBtn = 0;//控制筛选条件控制按钮是否显示
+        var textFilter = '';//筛选条件生成筛选标签的内容
+        //$scope.menusArr = [];//菜单数组
         $scope.componentList = [];//组件展示数组
-        function  menusEvent(obj,event){
-            //menusCliCount =1;
-            obj.on(event,function(){
-                var menusCliCount =1;
-                menusCliCount++;
-                $(this).addClass('active').siblings().removeClass('active');
-                if($(this).has('.menus-childs')){
-                    $(this).find('.menus-childs').slideDown().parent().siblings().find('.menus-childs').slideUp();
-                }
-            })
-        }
+        $scope.isType = true;
+        $scope.isStyle = true;
+        $scope.isBrand = true;
+        //function  menusEvent(obj,event){
+        //    //menusCliCount =1;
+        //    obj.on(event,function(){
+        //        var menusCliCount =1;
+        //        menusCliCount++;
+        //        $(this).addClass('active').siblings().removeClass('active');
+        //        if($(this).has('.menus-childs')){
+        //            $(this).find('.menus-childs').slideDown().parent().siblings().find('.menus-childs').slideUp();
+        //        }
+        //    })
+        //}
         /*
          * 分页器
          * */
-
-        //function getInitPagination(){
         $scope.totalItems = 64;
         $scope.currentPage = 1;
         $scope.setPage = function (pageNo) {
@@ -41,7 +44,6 @@ angular.module('core').controller('largePatternCtrl', ['$scope', '$http','$uibMo
         $scope.maxSize = 5;
         $scope.bigTotalItems = 175;
         $scope.bigCurrentPage = 1;
-        //}
         /*分页器跳转
          * params  value
          * return currentPage
@@ -50,11 +52,6 @@ angular.module('core').controller('largePatternCtrl', ['$scope', '$http','$uibMo
             dumpVal =  $('.dump-inp input').val();
             return dumpVal;
 
-        }
-        function setFilterStyle(obj,event){
-            obj.on(event,function(){
-                $(this).parent().siblings().css({'height':'auto','overflow':''})
-            })
         }
         /*分页器跳转
          * params  value
@@ -72,43 +69,148 @@ angular.module('core').controller('largePatternCtrl', ['$scope', '$http','$uibMo
          * */
         function setFilterStyle(obj){
             obj.on('mouseenter',function(){
-                $(this).children().css({'color':'blue','border-color':'blue'});
+                $(this).children().not('.filter-closeStatus').css({'color':'#E5383C','border-color':'#E5383C'});
                 $(this).children().find('.glyphicon-menu-up').css({'transform':'rotate(180deg)'});
-                $(this).find('.filter-trigger').css({'height':'33px','border-bottom':'0'});
+                $(this).not('.filter-closeStatus').find('.filter-trigger').css({'height':'33px','border-bottom':'0','background':'#fff'});
                 $(this).find('.switch-filter').show();
-            })
+            });
             obj.on('mouseleave',function(){
                 $(this).children().css({'color':'','border-color':''});
                 $(this).children().find('.glyphicon-menu-up').css({'transform':''});
-                $(this).find('.filter-trigger').css({'height':'30px','border-bottom':'1px solid #000'});
+                $(this).find('.filter-trigger').css({'height':'30px','border-bottom':'1px solid #c9c9c9','background':''});
                 $(this).find('.switch-filter').hide();
             })
         }
 
-        //getInitPagination();
+        /*
+         * 筛选条件生成条件标签
+         * params function
+         * */
+        function setFilterBlank(textFilter){
+            $('.filter-status .filter-ele').append('<b class="glyphicon glyphicon-menu-right"></b><div class="type-filter"><div class="filter-trigger ">'+textFilter+' <b class="glyphicon glyphicon-menu-up"></b> </div><div class="switch-filter" ><div>沙发</div><div>沙发</div><div>沙发</div><div>沙发</div></div></div>');
+        }
+        /*
+         * 关闭筛选条件
+         * */
+        function closeStatus(){
+            $('.filter-closeStatus').click(function(){
+                $(this).parent().prev().remove();
+                $(this).parent().remove();
+                $scope.isBrand = true;
+            })
+        }
+        /*
+         * 清空筛选条件
+         * */
+        //筛选品牌
+        $scope.isBrandFilter = function(event){
+            if(event.target.nodeName=='SPAN' || event.target.nodeName=='span') {
+                textFilter = $(event.target).text();
+                $('.filter-status .filter-ele').append('<b class="glyphicon glyphicon-menu-right"></b><div class="type-filter"><div class="filter-trigger filter-closeStatus">' + textFilter + '<b class="icon-close"></b></div>');
+                $scope.isBrand = false;
+            }
+            closeStatus();
+        };
+        //筛选风格
+        $scope.isStyleFilter = function(event){
+            if(event.target.nodeName=='SPAN' || event.target.nodeName=='span') {
+                textFilter = $(event.target).text();
+                setFilterBlank(textFilter);
+                setFilterStyle($('.component-base .filter-status .filter-ele .type-filter'));//筛选条件切换
+                $scope.isStyle = false;
+            }
+        };
+        //筛选类型
+        $scope.isTypeFilter = function(event){
+            if(event.target.nodeName=='SPAN' || event.target.nodeName=='span'){
+                textFilter = $(event.target).not('input[type="checkbox"]').text();
+                setFilterBlank(textFilter);
+                setFilterStyle($('.component-base .filter-status .filter-ele .type-filter'));//筛选条件切换
+                $scope.isType = false;
+            }
+
+        };
+        //取消筛选
+        $scope.cancelFilter = function(){
+            if($('.filter-trigger').length){
+                $scope.isBrand = true;
+                $scope.isStyle = true;
+                $scope.isType = true;
+            }
+            $('.type-filter').remove();
+            $('.filter-status .filter-ele>b').remove();
+        };
+        //多选
         //监听是否 菜单选项repeat 完成
         $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
             //菜单点击事件
             menusEvent($('.main-siderbar ul>li'),'click');
-            console.log($('.largePattern .component-info ul li .preview-small span'));
-            $('.largePattern .component-info ul li .preview-small span').hover(function(){
+            $('.component-info ul li .preview-small span').hover(function(){
                 var previewSrc = $(this).find('img').attr('src');
                 $(this).parent().siblings().find('img').attr('src',previewSrc)
             });
-        });
+            //菜单选项
+            $('.main-siderbar ul li p').click(function(){
+                var menusText = $(this).text();//选中的当前项的内容
+                $('.filter-status .filter-ele div').eq(0).html(menusText)//把值改变到筛选条件的路径监听框
+                $('p').css({'background':'','color':'#333'});//初始化样式
+                $('.glyphicon-menu-up').css({'transform':'rotate(0deg)'});
+                $(this).parent().children().find('p').css({'background':'','color':'#333'});//隐藏父元素的选中样式
+                $(this).css({'color':'#4990e2'});//选中样式
+                $(this).find('.glyphicon-menu-up').css({'transform':'rotate(180deg)'})
 
+            });
+            $('.filter-infoList ').css({'height':'50px','overflow':'hidden'});
+            $('.filter-infoList .filter-tool').map(function(i,val){
+                $('.checkMore').click(function(){
+                    $(this).parent().parent().find('.check-box').show();
+                    $(this).parent().parent().parent().css({'height':'auto','overflow':''});
+                    $(this).parent().parent().find('.btns-item').css({'display':'block'});
+                })
+            });
+            //是否选中筛选多选框 选中的话可以提交
+            $('.filter-infoList .check-box input').click(function(event){
+                showBtn=0;
+                var i=0;
+                $('.filter-infoList .check-box').map(function(i,val){
+                    if($(val).find('input[type="checkbox"]').prop('checked')==true){
+                        i=1;
+                        showBtn= i;
+                    }
+                });
+                if(showBtn==0){
+                    $(this).parent().parent().siblings().find('.btn-ok').hide();
+                }else{
+                    $(this).parent().parent().siblings().find('.btn-ok').show();
+                }
+            });
+            //筛选条件点击更多显示全部
+            $('.filter-tool ').map(function(){
+                $(this).find('.filter-more').unbind('click').click(function(){
+                    $(this).toggleClass('showMore');
+                    if($(this).hasClass('showMore')){
+                        $(this).parent().parent().parent().css({'height':'auto','overflow':''});
+                        $(this).find('.glyphicon-menu-up').css({'transform':'rotate(0deg)'});
+                    }else{
+                        $(this).parent().parent().parent().css({'height':'50px','overflow':'hidden'});
+                        $(this).find('.glyphicon-menu-up').css({'transform':'rotate(180deg)'});
+                    }
+                });
+            });
+            //  取消选择的时候条件清空
+            $('.filter-infoList .btn-cancel').click(function(){
+                $(this).parent().siblings().find('.check-box').hide();
+                $(this).parent().css({'display':'none'});
+                $(this).parent().siblings().find('input[type="checkbox"]').prop('checked','');
+                $scope.isType = true;
+                $scope.isStyle = true;
+                $scope.isBrand = true;
+            });
+        });
         /*
          *筛选状态
          * */
         $scope.typeFilter  = ['单人沙发','多人沙发','沙发','沙发沙发'];
-
-        //setFilterStyle($('.filter-condition .filter-ele>span'),'click')
-        $('.filter-condition .filter-ele>span').click(function(){
-            $('.filter-status .filter-ele').append('<b class="glyphicon glyphicon-menu-right"></b><div class="type-filter"><div class="filter-trigger">'+$(this).text()+' <b class="glyphicon glyphicon-menu-up"></b> </div><div class="switch-filter" ><div>沙发</div><div>沙发</div><div>沙发</div><div>沙发</div></div></div>');
-            $(this).parent().parent().parent().hide();
-            setFilterStyle( $('.component-base .filter-status .filter-ele .type-filter'),'mouseenter');
-            setFilterStyle( $('.component-base .filter-status .filter-ele .type-filter'),'mouseleave');
-        });
         /*
          * .filter-down更多选项
          * */
@@ -123,51 +225,9 @@ angular.module('core').controller('largePatternCtrl', ['$scope', '$http','$uibMo
             if($(this).hasClass('activeShow')){
                 $('.filter-brand').show()
             }else{
-
                 $('.filter-brand').hide()
             }
         });
-        //筛选条件点击更多显示全部
-        $('.filter-infoList ').css({'height':'50px','overflow':'hidden'});
-        $('.filter-tool .filter-more').click(function(){
-            $(this).parent().parent().parent().css({'height':'auto','overflow':''})
-        })
-        //筛选条件多选
-        $('.filter-infoList .filter-tool').map(function(i,val){
-            $(this).find('.checkMore').click(function(){
-                $(this).parent().parent().find('.check-box').show();
-                $(this).parent().parent().parent().css({'height':'auto','overflow':''});
-                $(this).parent().parent().find('.btns-item').css({'display':'block'});
-            });
-        });
-        //是否选中筛选多选框 选中的话可以提交
-
-        var  showBtn = 0;
-        $('.filter-infoList .check-box>input').click(function(event){
-            showBtn=0;
-            var i=0;
-            $('.filter-infoList .check-box').map(function(i,val){
-                console.info($(val))
-                //console.log();
-                //console.info($(this).parent().find('input[type="checkbox"]').prop('checked'))
-                if($(val).find('input[type="checkbox"]').prop('checked')==true){
-                    i=1;
-                    showBtn= i;
-                }
-            });
-            if(showBtn==0){
-                $(this).parent().parent().siblings().find('.btn-ok').hide();
-            }else{
-                $(this).parent().parent().siblings().find('.btn-ok').show();
-            }
-        });
-        //  取消选择的时候条件清空
-        $('.filter-infoList .btn-cancel').click(function(){
-            $(this).parent().siblings().find('.check-box').hide();
-            $(this).parent().css({'display':'none'});
-            $(this).parent().siblings().find('input[type="checkbox"]').prop('checked','');
-        });
-
 
         /*
          * 初始化模态框
